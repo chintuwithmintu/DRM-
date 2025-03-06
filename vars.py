fb0306de9cf565c76e34811730bf7612191f11e0
chintuwithmintu <mcacourse01@gmail.com>
2025-03-06 21:14:36 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,6 @@
+# Don't Remove Credit Telegram - @Engineers_Babu
+from os import environ
+
+API_ID = int(environ.get("API_ID", "28817205"))
+API_HASH = environ.get("API_HASH", "f319d02866bf7b83e4de31002f6ba8a3")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7930856596:AAFxEm9xu7Bzx11jZ9G56Gng1qmpE781um4")
